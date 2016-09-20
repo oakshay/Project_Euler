3053e3f0962347d9bf7b6f2ef3dc5d6dcb2f8fab
Aditya Durvasula <d4adi@10.148.141.157>
2016-09-19 20:46:18 -0500

    add some solutions

---
+++ src/solutions/Problem2.java
@@ -0,0 +1,20 @@
+package solutions;
+
+public class Problem2 {
+	static int sum = 0;
+	
+	public static void main(String args[]) {
+		fib(1, 1);
+		System.out.println(sum);
+	}
+	
+	public static void fib(int first, int second) {
+		if(second >= 4000000) {
+			return;
+		}
+		if(second % 2 == 0) {
+			sum += second;
+		}
+		fib(second, second + first);
+	}
+}
---
+++ src/solutions/Problem3.java
@@ -0,0 +1,27 @@
+package solutions;
+
+public class Problem3 {
+	public static void main(String args[]) {
+		long num = 600851475143L;
+		System.out.println(getLargestPrimeFactor(num));
+	}
+	public static long getLargestPrimeFactor(long num) {
+		long maxPrimeFactor = 1;
+		for(long i = 3; i < Math.sqrt(num); i+=2) {
+			if(num % i == 0) {
+				if(isPrime(i)) {
+					maxPrimeFactor = i;
+				}
+			}
+		}
+		return maxPrimeFactor;
+	}
+	public static boolean isPrime(long num) {
+		for(int i = 3; i < Math.sqrt(num); i+=2) {
+			if(num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
